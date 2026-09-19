dc1642539fa15622a03b2c3b03c4db50fd2764f7
KwachOjunga <ojungakwach@gmail.com>
2026-09-19 18:36:51 +0300

    feat: add the ability to specify dialects one needs

--- src/lib.rs
+++ src/lib.rs
@@ -3,13 +3,38 @@
 
 //! Hardware dialects for [pliron].
 
+#[cfg(feature = "comb")]
 pub mod comb;
+#[cfg(feature = "hw")]
 pub mod hw;
+#[cfg(feature = "seq")]
 pub mod seq;
+#[cfg(feature = "sv")]
 pub mod sv;
 
 use pliron::context::Context;
 
+#[cfg(all(feature = "comb", not(feature = "default")))]
+pub fn register_comb(ctx: &mut Context) {
+    comb::register(ctx);
+}
+
+#[cfg(all(feature = "hw", not(feature = "default")))]
+pub fn register_hw(ctx: &mut Context) {
+    hw::register(ctx);
+}
+
+#[cfg(all(feature = "seq", not(feature = "default")))]
+pub fn register_seq(ctx: &mut Context) {
+    seq::register(ctx);
+}
+
+#[cfg(all(feature = "sv", not(feature = "default")))]
+pub fn register_sv(ctx: &mut Context) {
+    sv::register(ctx);
+}
+
+#[cfg(feature = "default")]
 /// Register all hardware dialects in the given [Context].
 pub fn register_all(ctx: &mut Context) {
     hw::register(ctx);
